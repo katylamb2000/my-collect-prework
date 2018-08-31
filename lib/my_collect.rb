84079863773dbafe22eaad706372397a67145732
katy@karstaway.com <katy@karstaway.com>
2018-08-31 21:54:45 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,29 @@
+def my_collect (array)
 
+	 if block_given?
+	  
+  i = 0
 
+ while i < array.length
+	 yield(array[i])
+i = i + 1
+end
+end
+end
+
+students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
+
+def students(students) 
+  my_collect(students) do |student|
+       student.split(" ")
+    end
+  end
+    
+languages =   ['ruby', 'javascript', 'python', 'objective-c']
+ 
+ def lang(languages)
+ my_collect(languages) do |language|
+    puts language.upcase
+    end
+end
+ 
